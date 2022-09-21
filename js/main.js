628adad38ac092f7b73656bd7f52566f65244a46
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-09-21 17:17:30 -0300

    adicionando logica e demais botoes de trilha

--- js/main.js
+++ js/main.js
@@ -5,18 +5,46 @@ const play = document.querySelector ('.play');
 
 const pause = document.querySelector ('.pause');
 
-const trilha = document.querySelector ('.musica');
+const musica1 = document.querySelector ('.musica');
+
+const musica2 = document.querySelector ('.musica2');
+
+const proximo = document.querySelector ('.proximo');
+
+const anterior = document.querySelector ('.anterior');
 
 play.addEventListener ('click', () =>{
-    for (let index = 0; index < trilha.length; index++) {
-        const element = trilha[index];
-        
-    }
-        trilha.play();
+       musica1.play();
 });
+
+
+proximo.addEventListener ('click', () => {
+    
+        musica2.play();
+        musica1.pause ();
+        musica1.currentTime =0;      
+
+});
+
+anterior.addEventListener ('click', () => {
+    
+    musica1.play();
+    musica2.pause ();
+    musica2.currentTime =0       
+
+});
+
+
 
 
 pause.addEventListener ('click', () => {
-        trilha.currentTime = 0;
-        trilha.pause ();
+        musica1.currentTime = 0;
+        musica2.currentTime = 0;
+        musica1.pause ();
+        musica2.pause ();
 });
+
+
+
+
+// frases 
